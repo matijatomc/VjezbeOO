c894b07370823205e4a1ab83c51367b48a3d4917
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-03-10 12:24:43 +0100

    Add files via upload

---
+++ tomcMatijaVjezbaOO2/tomcMatijaVjezbaOO2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaVjezbaOO2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vozila auto = new Vozila("Auto",4);
+            Console.WriteLine("Upisite max brzinu auta: ");
+            auto.MaxBrzina=  Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ tomcMatijaVjezbaOO2/tomcMatijaVjezbaOO2/Vozila.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaVjezbaOO2
+{
+    class Vozila
+    {
+        string vrsta;
+        int maxBrzina;
+        int brojKotaca;
+
+        public override string ToString()
+        {
+            string ispis = "\nVrsta: " + vrsta + "\nMax brzina: " + maxBrzina + " kmh" + "\nBroj kotača: " + brojKotaca;
+            return ispis;
+        }
+        public string Vrsta { get => vrsta; set => vrsta = value; }
+        public int MaxBrzina { get => maxBrzina; set => maxBrzina = value; }
+        public int BrojKotaca { get => brojKotaca; set => brojKotaca = value; }
+
+        public Vozila(string vrsta, int brojKotaca)
+        {
+            this.vrsta = vrsta;
+            this.brojKotaca = brojKotaca;
+        }
+    }
+}
