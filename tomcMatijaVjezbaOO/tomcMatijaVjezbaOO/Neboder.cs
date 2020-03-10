9cf02bc039b01fb10c60121d977c6b158c3b9e66
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-03-10 11:44:30 +0100

    Add files via upload

---
+++ tomcMatijaVjezbaOO/tomcMatijaVjezbaOO/Neboder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaVjezbaOO
+{
+    class Neboder
+    {
+        float visina;
+        int brojKatova;
+
+        public override string ToString()
+        {
+            string ispis = "\nVisina: " + visina + " m" + "\nBroj katova " + brojKatova ;
+            return ispis;
+        }
+        public float getVisina()
+        {
+            return visina;
+        }
+        public void setVisina(float visina)
+        {
+            this.visina = visina;
+        }
+
+        public int getBrojKatova()
+        {
+            return brojKatova;
+        }
+        public void setBrojKatova(int brojKatova)
+        {
+            this.brojKatova = brojKatova;
+        }
+
+        public Neboder(float visina, int brojKatova)
+        {
+            this.visina = visina;
+            this.brojKatova = brojKatova;
+        }
+    }
+}
---
+++ tomcMatijaVjezbaOO/tomcMatijaVjezbaOO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaVjezbaOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float prVisinaKata;
+            
+            Neboder Tower = new Neboder(200, 100);
+
+            Tower.setVisina(Tower.getVisina() + 10);
+            Tower.setBrojKatova(Tower.getBrojKatova()+1);
+
+            prVisinaKata = Tower.getVisina() / Tower.getBrojKatova();
+
+            Console.WriteLine(Tower.ToString() + "\nProsjećna visina kata " + prVisinaKata + "m");
+            Console.ReadKey();
+        }
+    }
+}
